65d644755c1abd4f9ef3e28d79bc52acf2b355ed
Dexter Yang <dextery991@gmail.com>
2019-06-22 19:43:22 +0100

    (8, 8, 8, 8)
    1. drop_rate 0.5
    2. compression 0.8
    3. avgpool_size 4

--- cluster_experiment_scripts/cifar10_arg_parsing_densenet_se_4.sh
+++ cluster_experiment_scripts/cifar10_arg_parsing_densenet_se_4.sh
@@ -39,7 +39,7 @@ cd ..
 python train_evaluate_emnist_classification_system.py --batch_size 100 --continue_from_epoch -1 --seed 0 \
                                                       --image_num_channels 3 --image_height 32 --image_width 32 \
                                                       --num_epochs 100 --experiment_name 'cifar10_test_exp' \
-                                                      --growth_rate 24 --block_config "(8, 10, 12, 14)" --compression 0.5 \
+                                                      --growth_rate 24 --block_config "(8, 10, 12, 14)" --compression 1 \
                                                       --num_init_feature 24 --bn_size 8 --drop_rate 0.2 --avgpool_size 8 \
                                                       --reduction 1 --dataset_name 'cifar10'\
                                                       --use_gpu "True" --gpu_id "0" --weight_decay_coefficient 0.00001
