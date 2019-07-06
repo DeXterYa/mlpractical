9e646c81ff68b78d4ea66424a13bc6834f1625d4
Dexter Yang <dextery991@gmail.com>
2019-07-06 23:44:23 +0100

    changed to max_pool

--- cluster_experiment_scripts/cifar10_arg_parsing_densenet_se_10.sh
+++ cluster_experiment_scripts/cifar10_arg_parsing_densenet_se_10.sh
@@ -38,10 +38,10 @@ cd ..
 
 python train_evaluate_emnist_classification_system.py --batch_size 50 --continue_from_epoch -1 --seed 0 \
                                                       --image_num_channels 3 --image_height 32 --image_width 32 \
-                                                      --num_epochs 100 --experiment_name 'cifar10_test_exp_4_1' \
+                                                      --num_epochs 100 --experiment_name 'cifar10_test_exp_4_4' \
                                                       --growth_rate 24 --block_config "(5, 5, 5, 5)" --compression 0.5 \
                                                       --num_init_feature 24 --bn_size 8 --drop_rate 0.2 --avgpool_size 8 \
-                                                      --attention_pooling_type 'avg_pool' --dataset_name 'cifar100'\
+                                                      --attention_pooling_type 'max_pool' --dataset_name 'cifar100'\
                                                       --attention_network_type 'relational_network' --attention_pooling_size 5\
                                                       --num_attention_layers 4  --num_attention_filters 50\
                                                       --use_gpu "True" --gpu_id "0" --weight_decay_coefficient 0.00001
