70f3711c9cd24bf7218193057deafac959d8da73
Dexter Yang <dextery991@gmail.com>
2019-07-09 20:13:35 +0100

    debug

--- cluster_experiment_scripts/result.sh
+++ cluster_experiment_scripts/result.sh
@@ -8,7 +8,7 @@ do
     for (( j = 1 ; j <= 3; j++ ))
     do
 
-       rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_${i}_${j}/result_outputs/test_summary.csv /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/train_7_9_2/test_${i}_${j}
+       rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_${i}_${j}/result_outputs/test_summary.csv /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/train_7_9_1/test_${i}_${j}
     done
 
 
