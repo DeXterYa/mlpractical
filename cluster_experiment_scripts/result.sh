abf4057b797c6c340cacb03d9672b5f81114927b
Dexter Yang <dextery991@gmail.com>
2019-07-10 20:05:21 +0100

    new train

--- cluster_experiment_scripts/result.sh
+++ cluster_experiment_scripts/result.sh
@@ -2,14 +2,14 @@
 
 cd ..
 ls
-for (( i = 1; i <= 4; i++ ))
+for (( i = 1; i <= 8; i++ ))
 do
 
     for (( j = 1 ; j <= 3; j++ ))
     do
-       rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_${i}_${j}/result_outputs/summary.csv /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/train_7_10_1/train_${i}_${j}
+       rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_${i}_${j}/result_outputs/summary.csv /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/train_7_10_2/train_${i}_${j}
 
-       rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_${i}_${j}/result_outputs/test_summary.csv /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/train_7_10_1/test_${i}_${j}
+       rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_${i}_${j}/result_outputs/test_summary.csv /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/train_7_10_2/test_${i}_${j}
     done
 
 
--- cluster_experiment_scripts/training_template_experiment_8_2.sh
+++ cluster_experiment_scripts/training_template_experiment_8_2.sh
@@ -38,12 +38,12 @@ cd ..
 
 python train_evaluate_emnist_classification_system.py --batch_size 50 --continue_from_epoch -1 --seed 1 \
                                                       --image_num_channels 3 --image_height 32 --image_width 32 \
-                                                      --num_epochs 100 --experiment_name "experiment_3_2" \
+                                                      --num_epochs 100 --experiment_name "experiment_8_2" \
                                                       --growth_rate 24 --block_config "(5, 5, 5, 5)" --compression 0.5 \
                                                       --num_init_feature 24 --bn_size 8 --drop_rate 0.2 --avgpool_size 8 \
                                                       --attention_pooling_type "avg_pool" --dataset_name "cifar100"\
-                                                      --attention_network_type "relational_network" --attention_pooling_size 5\
-                                                      --num_attention_layers 3  --num_attention_filters 50\
+                                                      --attention_network_type "fcc_network" --attention_pooling_size 3\
+                                                      --num_attention_layers 4  --num_attention_filters 50\
                                                       --conv_bn_relu_type_bottleneck "WeightAttentionalConv2dNormLeakyReLU" \
                                                       --conv_bn_relu_type_processing "Conv2dNormLeakyReLU" \
                                                       --use_gpu "True" --gpu_id "0" --weight_decay_coefficient 0.00001
