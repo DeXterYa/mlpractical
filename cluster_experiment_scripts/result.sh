fe604c0eaa620eb584a591c40231f41756338444
Dexter Yang <dextery991@gmail.com>
2019-07-20 10:13:49 +0100

    changed the result.sh and run.sh

--- cluster_experiment_scripts/result.sh
+++ cluster_experiment_scripts/result.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
 cd ..
-ls
-for (( i = 1; i <= 8; i++ ))
-do
-
-    for (( j = 1 ; j <= 3; j++ ))
-    do
-       rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_${i}_${j}/result_outputs/summary.csv /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/train_7_11_2/train_${i}_${j}
-
-       rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_${i}_${j}/result_outputs/test_summary.csv /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/train_7_11_2/test_${i}_${j}
-    done
-
-
-done
 
 
 
 
-
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_1_3_50_0_165 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_1_3_50_1_1317 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_1_3_50_2_2469 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_1_5_50_0_181 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_1_5_50_1_1333 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_1_5_50_2_2485 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_2_3_50_0_213 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_2_3_50_1_1365 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_2_3_50_2_2517 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_2_5_50_0_229 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_2_5_50_1_1381 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_2_5_50_2_2533 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_3_3_50_0_261 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_3_3_50_1_1413 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_3_3_50_2_2565 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_3_5_50_0_277 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_3_5_50_1_1429 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
+rsync -ua --progress s1891076@mlp.inf.ed.ac.uk:/home/s1891076/mlpractical/experiment_cifar100_HyperConv2dNormLeakyReLU_HyperConv2dNormLeakyReLU_avg_pool_fcc_network_3_5_50_2_2581 /afs/inf.ed.ac.uk/user/s18/s1891076/SummerInternshipTrainingResult/training_results_hyper
